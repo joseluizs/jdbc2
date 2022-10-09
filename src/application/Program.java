14d8df7075268e55c2955c0d346941ae70b1d4e5
joseluizs <joseluiz.smarques@gmail.com>
2022-10-09 09:17:02 -0300

    Insert Into Return ID

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,7 +24,8 @@ public class Program {
 					"INSERT INTO seller " 
 				  + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 				  + "VALUES "
-				  + "(?, ?, ?, ?, ?)");
+				  + "(?, ?, ?, ?, ?)",
+				  Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
@@ -33,7 +34,16 @@ public class Program {
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+				
+			}else {
+				System.out.println("No rown affected!");
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
